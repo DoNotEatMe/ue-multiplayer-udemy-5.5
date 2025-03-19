3cc4538e55d511a0e116bbc104a67bb65d7cc9b1
Igor Kabanov <udeveloper@rivestudio.ru>
2025-03-20 00:17:41 +0300

    2.7

---
+++ Source/MultiplayerEdu/Mybox.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mybox.h"
+
+// Sets default values
+AMybox::AMybox()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+	
+}
+
+// Called when the game starts or when spawned
+void AMybox::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		GEngine->AddOnScreenDebugMessage(-1,10.f,FColor::Red,"Server");		
+	} else
+	{
+		GEngine->AddOnScreenDebugMessage(-1,10.f,FColor::Green,"Client");
+	}
+	
+}
+
+// Called every frame
+void AMybox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
